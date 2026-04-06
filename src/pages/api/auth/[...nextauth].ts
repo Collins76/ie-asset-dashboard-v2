99d015b742b41f38a1545c3a3c93362b5a031f35
Collins A. <collins.tochi@gmail.com>
2026-04-06 13:42:33 +0100

    Use wellKnown for Google OIDC discovery

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,25 +1,17 @@
 import NextAuth from 'next-auth';
 
 export default NextAuth({
-  debug: true,
   providers: [
     {
       id: 'google',
       name: 'Google',
       type: 'oauth',
-      clientId: process.env.GOOGLE_CLIENT_ID || process.env.AUTH_GOOGLE_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || process.env.AUTH_GOOGLE_SECRET || '',
-      authorization: {
-        url: 'https://accounts.google.com/o/oauth2/v2/auth',
-        params: {
-          scope: 'openid email profile',
-          response_type: 'code',
-        },
-      },
-      token: 'https://oauth2.googleapis.com/token',
-      userinfo: 'https://openidconnect.googleapis.com/v1/userinfo',
+      clientId: process.env.GOOGLE_CLIENT_ID || process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || process.env.AUTH_GOOGLE_SECRET,
+      wellKnown: 'https://accounts.google.com/.well-known/openid-configuration',
+      authorization: { params: { scope: 'openid email profile' } },
       idToken: true,
-      checks: ['state'],
+      checks: ['pkce', 'state'],
       profile(profile: { sub: string; name: string; email: string; picture: string }) {
         return {
           id: profile.sub,
